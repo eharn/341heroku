d43b5226f2db1a9bca3d32329118728b7e1f07f6
eharn <77076617+eharn@users.noreply.github.com>
2022-02-09 21:34:26 -0700

    port change

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const path = require('path');
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -83,7 +83,7 @@ app.use(errorController.get404);
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-    app.listen(3000);
+    app.listen(PORT);
   })
   .catch(err => {
     console.log(err);
